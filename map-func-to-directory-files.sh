ef34b1640a4a2db1453c4c2a55e97684a6e012e5
RAONI MATOS SMANEOTO 116110112 <raonims@lcc3-019.lcc.ufcg.edu.br>
2019-04-23 11:38:55 -0300

    Mapper

---
+++ map-func-to-directory-files.sh
@@ -0,0 +1,23 @@
+#!/bin//bash
+
+if [ "$#" -lt 2 ]; then
+    echo "Illegal number of parameters"
+fi
+
+func=$1
+directory=$2
+filenames=`ls $2`
+
+optional_dir=$3
+
+if [ ! -z "$optional_dir" ] ; then
+    for filename in $filenames; do
+	$func $directory/$filename $optional_dir
+    done
+else
+    for filename in $filenames; do
+	$func $directory/$filename
+    done
+fi
+
+
