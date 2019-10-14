27a300f4357ff87d00dc22ee417fff9a9afb4ad8
Raoni Smaneoto <raoni.smaneoto@ccc.ufcg.edu.br>
2019-10-14 13:01:44 -0300

    mkcpp

---
+++ make-cpp-skel.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+file_name=$1
+touch $file_name
+
+echo "#include <bits/stdc++.h>" >> $file_name
+
+echo "" >> $file_name
+
+echo "using namespace std;" >> $file_name
+echo "typedef long long ll;" >> $file_name
+
+echo "" >> $file_name
+
+echo "int main() {" >> $file_name
+
+echo "  return 0;" >> $file_name
+
+echo "}" >> $file_name
+
+exit 0
