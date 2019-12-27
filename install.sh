9bdb8c73f307570f2be7def88f5890dfe05e6e40
raonismaneoto <raoni.smaneoto@vtex.com.br>
2019-12-27 16:31:57 -0300

    installation script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+scripts=`ls`
+
+for script in scripts
+do
+	filename=$(basename -- "$script")
+	filename="${filename%.*}"
+	mv $script /usr/local/bin/$filename
+	chmod +x /usr/local/bin/$filename
+done
+
+rm -rf ../useful_scripts
+
