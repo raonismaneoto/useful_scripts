2d85eb0fb5b2f26688b46028bd59ac6ef27da3a4
Raoni Smaneoto <raoni.smaneoto@ccc.ufcg.edu.br>
2019-12-27 17:25:52 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,10 @@ scripts=`ls`
 
 for script in $scripts
 do
+	if [[ "$script" == "install.sh" ]]; then
+		continue
+	fi
+	
 	filename=$(basename -- "$script")
 	filename="${filename%.*}"
 	mv $script /usr/local/bin/$filename
