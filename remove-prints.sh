43508b3decea2315d3d6e860c14a9df5bbcc1880
raonismaneoto <raoni.smaneoto@vtex.com.br>
2020-02-13 17:17:27 -0300

    remove-prints

---
+++ remove-prints.sh
@@ -0,0 +1,44 @@
+print_function=$1
+root_dir=$2
+recursive=$3
+
+exec_for_one_dir () {
+    dir=$1
+    files=`ls $dir`
+    print_function=$2
+    for file in $files
+    do
+        if [ -d $file ]; then
+            continue
+        fi
+
+        amount=`grep -c $print_function $file`
+        for ((i=1;i<=$amount;i++)); do
+            sed '/$print_function/d' $file
+        done
+    done
+}
+
+if [ -z $recursive ]; then
+    files=`ls $root_dir`
+    for file in $files
+    do
+        if [ -d $file ]; then
+            continue
+        fi
+
+        amount=`grep -c $print_function $file`
+        for ((i=1;i<=$amount;i++)); do
+            sed '/$print_function/d' $file
+        done
+    done
+else
+    files=`ls $root_dir`
+    exec_for_one_dir $root_dir $print_function
+    for file in $files
+    do
+        if [ -d $file ]; then
+            exec_for_one_dir $file $print_function
+        fi
+    done
+fi
